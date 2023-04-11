f1ad30d2c6ad86b36cb69f0927aeeb176b083090
greatraksin <greatraksin@icloud.com>
2023-04-11 18:48:28 +0300

    Adding DB insertion

--- bot_functions.py
+++ bot_functions.py
@@ -36,9 +36,14 @@ def button_message(message):
 @bot.message_handler(content_types=['contact', 'location'])
 def save_user(message):
     if message.contact is not None:  # если номер телефона передали
-        print(message.contact)
-        with open('users.txt', 'a', encoding='utf-8') as file:  # записываем инфу о пользователе в файл
-            file.write(f'{message.contact}\n')
+        connect = connect_db()
+        cont = message.contact
+        u = add_user(cont.first_name, cont.last_name, cont.user_id, cont.phone_number, connect)
+        if u is not None:
+            bot.send_message(message.chat.id, 'Спасибо!')
+        else:
+            bot.send_message(message.chat.id, 'Вы уже отправляли мне контакты!')
+
     elif message.location is not None:  # если локацию передали
         lat = message.location.latitude
         lon = message.location.longitude
